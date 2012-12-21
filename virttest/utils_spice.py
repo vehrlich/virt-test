199af7cafeeb3e617bc8814980c5820e3892f0e3
Vaclav Ehrlich <vehrlich@redhat.com>
2012-12-21 13:01:40 +0100

    utils_spice.py: Added shared method
    Added method used in rv_connect and in tests from future (rv_video_connect, ...)
    
    Signed-off-by: Vaclav Ehrlich <vehrlich@redhat.com>

--- virttest/utils_spice.py
+++ virttest/utils_spice.py
@@ -16,6 +16,37 @@ def wait_timeout(timeout=10):
     logging.debug("Waiting (timeout=%ss)", timeout)
     time.sleep(timeout)
 
+def launch_gnome_session(vm_session):
+    """
+    Launches gnome session inside VM session
+    @param vm_session - vm.wait_fo_login()
+
+    metacity ensures that newly raised window will be active
+    (remote-viewer auth dialog)
+    which is not done by default in pure Xorg
+    """
+    cmd = "nohup gnome-session --display=:0.0 &> /dev/null &"
+    return vm_session.cmd(cmd)
+
+def launch_xorg(vm_session):
+    """
+    Launches Xorg inside vm_session on background
+    @param vm_session - vm.wait_for_login()
+    """
+    cmd = "Xorg"
+    killall(vm_session, cmd)
+    wait_timeout() # Wait for Xorg to exit
+    cmd = "nohup " + cmd + " &> /dev/null &"
+    return vm_session.cmd(cmd)
+
+def killall(vm_session, pth):
+    """
+    calls killall execname
+    @params vm_session
+    @params pth - path or execname
+    """
+    execname = pth.split(os.path.sep)[-1]
+    vm_session.cmd("killall %s &> /dev/null" % execname, ok_status=[0, 1])
 
 def start_vdagent(guest_session, test_timeout):
     """
