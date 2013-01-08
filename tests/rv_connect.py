c487a3807619f1f56696500188bb8ea5f4e40a27
Vaclav Ehrlich <vehrlich@redhat.com>
2013-01-08 13:57:08 +0100

    rv_connect.py: running startx update
    Launching startx sometime caused a test error. X server and Gnome started successfully but test analyzed log output as error.
    Added error log redirction to /dev/null solved problem. Now test analyzes only PID.
    
    Signed-off-by: Vaclav Ehrlich <vehrlich@redhat.com>

--- tests/rv_connect.py
+++ tests/rv_connect.py
@@ -135,7 +135,7 @@ def launch_rv(client_vm, guest_vm, params):
 
     # Launching the actual set of commands
     try:
-        client_session.cmd("startx &", timeout=15)
+        client_session.cmd("startx &> /dev/null &", timeout=15)
     except (ShellCmdError, ShellStatusError):
         logging.debug("Ignoring an Exception that Occurs from calling startx")
 
