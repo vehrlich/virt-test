bbce177a46556c87df1f0c74a27e70115a171b70
Vaclav Ehrlich <vehrlich@redhat.com>
2013-02-26 18:25:09 +0100

    usb_redirection.py:USB redirection test
    Test launch quest vm with usb redirection support, client vm with emulated USB device.
    Tests copy of data from guest to USB and from USB to client.
    
    Signed-off-by: Vaclav Ehrlich <vehrlich@redhat.com>

--- tests/usb_redirection.py
+++ tests/usb_redirection.py
@@ -1,47 +1,47 @@
 """
-usb_insert.py - tests USB connection, file transfer
+usb_redirection.py - tests USB connection, file transfer
+
+There are two ways how to simulate USB plugin and redirection.
+
+One is with remote-viewer option --spice-usbredir-redirect-on-connect
+This option is not present in virt-viewer 0.5.2-4 in RHEL 6.3.
+But is used in virt-viewer 0.5.2-18 in RHEL 6.4. 
+
+Second is use qemu monitor command and add USB device after remote-viewer 
+connection.
+Unfortunately this command is not present in qemu-kvm 0.1.12 which is used in 
+RHEL 6.4.
+
+This means there is no way how to simulate USB redirection in RHEL 6.3. 
+In RHEL6.4 is possible to use remote-viewer option.
+In future it will be more easy with qemu-kvm monitor command
 """
+
 import os
 import logging
+from virttest import utils_net, utils_spice, remote
 
-def _create_usb_storage(session, size_of_storage):
+
+def _create_usb_storage(vm, storage_file, size_of_storage):
     """
     Method created USB storage with proper size and creates there file
     with content.
-    
-    @param session: VM's session 
-    @param size_of_storage: size of storage in MB 
+    USB device is created on host then is with monitor command attached to guest
+
+
+    @param session: VM's session
+    @param storage_file: emulated USB file
+    @param size_of_storage: size of storage in MB
     """
-    #clean possible image and mount point from last use
-    try:
-        session.cmd("mount | grep /mnt/usb")
-    except:
-        pass
-    else:
-        logging.debug("Umount /mnt/usb")
-        session.cmd("umount /mnt/usb")
-        
-    try:
-        session.cmd("ls /mnt/usb")
-    except:
-        pass
-    else:
-        logging.debug("Remove mount point")
-        session.cmd("rm -rf /mnt/usb")
 
-    try:
-        session.cmd("ls /tmp/usb.img")
-    except:
-        pass
-    else:
-        logging.debug("Remove image")
-        session.cmd("rm -rf /tmp/usb.img")
-    
-    logging.debug("Creating device /tmp/usb.img")
-    session.cmd("dd if=/dev/zero of=/tmp/usb.img bs=%sM count=1" % size_of_storage)
+    os.system("rm -rf %s" % storage_file)
+    logging.debug("Creating device %s" % storage_file)
+    os.system('dd if=/dev/zero of=%s bs=%sM count=1' % (storage_file,
+                                                size_of_storage))
+    logging.debug("Creating filesystem ext3 on %s" % storage_file)
+    os.system("mkfs.ext3 -F -L test -q %s " % storage_file)
 
-    logging.debug("Creating filesystem ext3 on /tmp/usb.img")
-    session.cmd("mkfs.ext3 -F -L test -q /tmp/usb.img")
+    vm.monitor.cmd("usb_add disk:%s" % storage_file)
 #enddef
 
 def _mount_usb_storage(session):
@@ -55,11 +55,31 @@ def _mount_usb_storage(session):
     session.cmd("echo 'This is the test' > /mnt/usb/test")
 #enddef
 
+
+def create_random_file(session, path, size):
+    """
+    Creates file with path size with random data.
+    Returns md5hash of this file.
+
+    @param session: VM session
+    @param path: path to file
+    @param size: size of file
+
+    @return md5hash of file
+    """
+    session.cmd("dd if=/dev/random of=%s bs=%s count=1" % (path, size))
+    session.cmd("md5sum %s" % path)
+
 def run_usb_redirection(test, params, env):
     """
     Test for USB connection
-    
-    @param test: KVM test object.  
+    Storage for USB device is created in qemu_vm.py. Then is
+    guest VM started with USB redirection options and configuration
+    files. Client VM is started with emulated USB device.
+
+    This test copy data from client to guest in both ways.
+
+    @param test: KVM test object.
     @param params: Dictionary with the test parameters.
     @param env: Dictionary with test environment.
     """
@@ -72,7 +92,26 @@ def run_usb_redirection(test, params, env):
     client_vm.verify_alive()
     client_session = client_vm.wait_for_login(
             timeout=int(params.get("login_timeout", 360)))
-
-    #create usb storage
-    #_create_usb_storage(client_session, params.get("storage_size", 1))
-    #_mount_usb_storage(client_session)
+    utils_spice.launch_startx(guest_vm)
+    utils_spice.wait_timeout(10)
+    #create file on guest
+    guest_session.cmd("dd if=/dev/random of=%s bs=%s count=1" % ("/tmp/test.file", 4))
+    md5sum_guest = guest_session.cmd("md5sum %s" % "/tmp/test.file")
+    logging.info("MD5SUM on guest: %s" % md5sum_guest)
+    #copy file on USB
+    guest_session.cmd("cp %s %s" % ("/tmp/test.file", "/media/test/test.file"))
+    md5sum_guest_usb = guest_session.cmd("md5sum %s" % "/media/test/test.file")
+    logging.info("MD5SUM on guest USB: %s" % md5sum_guest_usb)
+    #disconnect USB (kill remote-viewer)
+    rv_id = guest_session.cmd("pgrep remote-viewer")
+    guest_session.cmd("pkill %s" % rv_id)
+    #check md5sum on client USB
+    md5sum_client_usb = client_session.cmd("md5sum %s" % "/media/test/test.file")
+    logging.info("MD5SUM on client USB: %s" % md5sum_client_usb)
+    #copy file to client
+    client_session.cmd("cp %s %s" % ("/media/test/test.file", "/tmp/test.file"))
+    #check md5sum 
+    md5sum_client = client_session.cmd("md5sum %s" % "/tmp/test.file")
+    logging.info("MD5SUM on client: %s" % md5sum_client)
+    
+    
