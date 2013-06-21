b99cc52305b5fdbc3e7bd3536024faf02c5657e5
Vaclav Ehrlich <vehrlich@redhat.com>
2013-06-21 17:04:13 +0200

    usb_redirection.py: USB redirection test
    Tests USB redirection from client to guest.
    On guest is created file which is copied to USB.
    USB is then unplugged and plugged into client.
    File is copied to client.
    
    If md5sun of all files is the same - then returns True
    
    Signed-off-by: Vaclav Ehrlich <vehrlich@redhat.com>

--- tests/usb_redirection.py
+++ tests/usb_redirection.py
@@ -12,47 +12,14 @@ connection.
 Unfortunately this command is not present in qemu-kvm 0.1.12 which is used in 
 RHEL 6.4.
 
-This means there is no way how to simulate USB redirection in RHEL 6.3. 
-In RHEL6.4 is possible to use remote-viewer option.
-In future it will be more easy with qemu-kvm monitor command
+This means there is no way how to simulate USB redirection in RHEL 6.3. In 
+RHEL6.4 is possible to use remote-viewer option. In future it will be more easy 
+with qemu-kvm monitor command
 """
 
 import logging
-from virttest import utils_spice
-
-def check_usb_policy(vm, params):
-    """
-    Check USB policy in polkit file
-    """    
-    logging.info("Checking USB policy")
-    file = '/usr/share/polkit-1/actions/org.spice-space.lowlevelusbaccess.policy'
-    cmd = "grep \"<allow_all>yes\" " + file + " & > /dev/null"
-    logging.info("CMD : %s" % cmd)
-    client_root_session = vm.wait_for_login(
-            timeout=int(params.get("login_timeout", 360)),
-            username="root", password="123456")
-    usb_policy = client_root_session.cmd_status(cmd)
-    logging.info("Policy %s" % usb_policy)
-    return usb_policy
-
-def add_usb_policy(vm, params):
-    """
-    Add USB policy to policykit file
-    """
-    logging.info("Adding USB policy")
-    file = "/usr/share/polkit-1/actions/org.spice-space.lowlevelusbaccess.policy"
-    client_root_session = vm.wait_for_login(
-            timeout=int(params.get("login_timeout", 360)),
-            username="root", password="123456")
-    #get line number of old definition
-    cmd = "grep -n \"<allow_active>\" %s | cut -d : -f 1" % file
-    line_number = client_root_session.cmd_output(cmd)
-    
-    logging.info(line_number)
-    
-    #put new definition after this line number
-    #cmd = "(echo '%sa'; echo '<allow_all>yes</allow_all>'; echo 'wq') | ed -s %s" % (line_number, file)
-    #client_root_session.cmd(cmd)
+from virttest import utils_misc, utils_spice
+from autotest.client.shared import error
 
 
 def run_usb_redirection(test, params, env):
@@ -62,7 +29,9 @@ def run_usb_redirection(test, params, env):
     guest VM started with USB redirection options and configuration
     files. Client VM is started with emulated USB device.
 
-    This test copy data from client to guest in both ways.
+    This test copy data from guest to client through USB.
+    
+    Test expects remote-viewer running (rv_connect)
     
     #TODO paths are hardcoded this will be problem with Windows USB redir. Make
     #it more sophisticated.
@@ -71,6 +40,7 @@ def run_usb_redirection(test, params, env):
     @param params: Dictionary with the test parameters.
     @param env: Dictionary with test environment.
     """
+    #prepare guest and client sessions
     guest_vm = env.get_vm(params["guest_vm"])
     guest_vm.verify_alive()
     guest_session = guest_vm.wait_for_login(
@@ -81,38 +51,40 @@ def run_usb_redirection(test, params, env):
     client_session = client_vm.wait_for_login(
             timeout=int(params.get("login_timeout", 360)))
     
-    if not check_usb_policy(client_vm, params):
-        logging.info("No USB policy")
-        add_usb_policy(client_vm, params)
-    else:
-        logging.info("USB policy OK")
-
+    client_root_session = client_vm.wait_for_login(
+            timeout=int(params.get("login_timeout", 360)), 
+            username="root", password="123456")
     
-    #create file on guest
+    #create file on guest 
     guest_session.cmd("dd if=/dev/random of=%s bs=%s count=1" %
                       ("/tmp/test.file", "4M"))
-    md5sum_guest = guest_session.cmd("md5sum /tmp/test.file")
+    md5sum_guest = guest_session.cmd("md5sum /tmp/test.file | cut -f1 -d\" \"")
     logging.info("MD5SUM on guest: %s" % md5sum_guest)
-    #copy file from guest to USB
+    #copy file from guest to USB(USB is mounted automaticaly to /media )
     guest_session.cmd("cp %s %s" % ("/tmp/test.file", "/media/test/test.file"))
-    md5sum_guest_usb = guest_session.cmd("md5sum /media/test/test.file")
+    md5sum_guest_usb = guest_session.cmd("md5sum /media/test/test.file | cut -f1 -d\" \"")
     logging.info("MD5SUM on guest USB: %s" % md5sum_guest_usb)
-    #disconnect USB (kill remote-viewer)
+    #disconnect USB
     guest_session.cmd("umount /media/test")
+    #USB hold by guest is freed after VM shutdown (maybe bug? Should not be closing 
+    #client enough?)
+    guest_vm.destroy()
     client_session.cmd("pkill remote-viewer")
     utils_spice.wait_timeout(10)
     #check md5sum on client USB
-    md5sum_client_usb = client_session.cmd("md5sum /media/test/test.file")
+    md5sum_client_usb = client_session.cmd("md5sum /media/test/test.file | cut -f1 -d\" \"")
     logging.info("MD5SUM on client USB: %s" % md5sum_client_usb)
     #copy file to client
     client_session.cmd("cp %s %s" % ("/media/test/test.file", "/tmp/test.file"))
     #check md5sum 
-    md5sum_client = client_session.cmd("md5sum /tmp/test.file")
+    md5sum_client = client_session.cmd("md5sum /tmp/test.file | cut -f1 -d\" \"")
     logging.info("MD5SUM on client: %s", md5sum_client)
-    
-    if md5sum_guest == md5sum_guest_usb ==  md5sum_client_usb ==  md5sum_client:
+
+    if md5sum_guest == md5sum_guest_usb == md5sum_client_usb ==  md5sum_client:
         logging.info("MD5SUM check PASS")
         return True
     else:
-        logging.info("MD5SUM check FAILED")
-        return False
+        raise error.TestFail("MD5SUM check FAILED")
+    
+    client_session.close()
+    guest_session.close()
